4c251e31061976ce02caa6529205ab02a4d3c6d9
ahmedirheem <ahmedirheem2003@gmail.com>
2022-07-26 23:10:12 +0300

    Js Folder

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 const aside = document.querySelector("aside")
 const main = document.querySelector("main")
-const muneBtn = document.querySelector(".mune_btn")
+const menuBtn = document.querySelector(".menu_btn")
 
-muneBtn.addEventListener("click", ()=>{
+menuBtn.addEventListener("click", ()=>{
     main.classList.toggle("full")
     aside.classList.toggle("full")
 })
+
+// Start Card 
+
+let mainContent = document.querySelector(".main_content")
+
+for(let i = 0; i < data.length; i++){
+mainContent.innerHTML += 
+`
+    <div class="card">
+        <div class="video_bannar">
+            <a href="#">
+                <img class="bannar" src="${data[i].banar}" alt="">
+                <img class="preview" src="${data[i].preview}" alt="">
+            </a>
+            <span>${data[i].time}</span>
+        </div>
+        <div class="info">
+            <img class="logo" src="${data[i].avatar}" alt="${data[i].chanle}">
+            <div class="text">
+                <p class="video_title">${data[i].title}</p>
+                <span class="channel" title="${data[i].chanle}">${data[i].chanle}</span>
+                <span>${data[i].views} views . ${data[i].date}</span>
+            </div>
+        </div>     
+    </div>
+`
+}
